1c61ba5e8e89221bb42427f73fd5314bf7603836
shanmugapriya2912 <shanmugapriyas2912@gmail.com>
2026-09-23 19:53:53 +0530

    Update firewall_services.sh

--- firewall_services.sh
+++ firewall_services.sh
@@ -9,34 +9,28 @@
 
 
 # Check current firewall services
-
-
+firewall-cmd --list-services
 
 
 # Allow HTTP service
-
+firewall-cmd --add-service=http
 
 
 # Allow HTTPS service
-
-
+firewall-cmd --add-service=https
 
 
 # Display enabled services
-
-
+firewall-cmd --list-services
 
 
 # Remove HTTP service
-
-
+firewall-cmd --remove-service=http
 
 
 # Allow SSH permanently
-
-
+firewall-cmd --permanent --add-service=ssh
 
 
 # Reload firewall configuration
-
-
+firewall-cmd --reload
